4c5067a6068ed47420598753b8d3eff137d0b702
Dr. Andreas Plagens <andreas@plagens.eu>
2025-04-04 14:56:16 +0200

    Initial Commit

---
+++ Primzahl7/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Primzahl7
+//
+//  Created by Dr. Andreas Plagens on 04.04.25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
